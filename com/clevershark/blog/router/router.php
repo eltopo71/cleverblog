43d8b717c64507ed0421d788be92ed5557b8ccd1
eltopo71 <clevershark@gmail.com>
2014-06-12 01:21:25 -0300

    OO blog written in PHP
    Initial commit

---
+++ com/clevershark/blog/router/router.php
@@ -0,0 +1,17 @@
+<?php
+class router {
+	// the router receives the web request, breaks it down, then 
+	// passes it on to the appropriate controller
+	
+	private $getVars;
+	private $postVars;
+	private $cookieVars;
+	private $urlPath;
+	private $sessionVars;
+	private $envVars;
+	private $errorVars;
+	
+	
+	
+}
+?>
